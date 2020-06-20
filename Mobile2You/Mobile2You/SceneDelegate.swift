364199d5501015de056b58b4d32ab21c4410c51d
KaioReis <kacio@premiersoft.net>
2020-06-20 09:37:58 -0300

    init project

---
+++ Mobile2You/Mobile2You/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  Mobile2You
+//
+//  Created by Kacio Batista on 20/06/20.
+//  Copyright © 2020 Kacio Batista. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController()
+        self.window = window
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window?.windowScene = windowScene
+        self.window?.makeKeyAndVisible()
+    }
+    
+}
+
---
+++ Mobile2You/Mobile2YouTests/Mobile2YouTests.swift
@@ -0,0 +1,34 @@
+//
+//  Mobile2YouTests.swift
+//  Mobile2YouTests
+//
+//  Created by Kacio Batista on 20/06/20.
+//  Copyright © 2020 Kacio Batista. All rights reserved.
+//
+
+import XCTest
+@testable import Mobile2You
+
+class Mobile2YouTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
